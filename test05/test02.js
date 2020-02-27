b7771ef94dd6afb6f07877598289ae2ef135230a
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-02-27 09:53:27 +0000

    added test05

---
+++ test05/test02.js
@@ -0,0 +1,11 @@
+/*
+実行結果
+red
+blue
+yellow
+purple
+green
+*/
+
+var data = ['red','blue','yellow','purple','green'];
+
---
+++ test05/test04.js
@@ -0,0 +1,13 @@
+/*
+実行結果
+title => Beauty and Beast
+price => 1700
+genre => Animation
+*/
+
+var movie = {
+    title : "Beauty and the Beast",
+    price : 1700,
+    genre : "Animation",
+};
+
